282b3e318d60643442fad2252c9c77c5e0d68be9
Thilo Plasger <ekzodus4.17@gmail.com>
2026-01-07 21:06:15 +0100

    Add gin-gonic

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
 
 func main() {
 	fmt.Print("hi")
+	connStr := "user=pqgotest dbname=pqgotest sslmode=verify-full"
+	_, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
