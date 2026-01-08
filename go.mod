d7579b9f141942ac434202f9db93b6ac04472275
Thilo Plasger <ekzodus4.17@gmail.com>
2026-01-08 20:17:54 +0100

    Fix unconnectedness

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/goccy/go-yaml v1.19.1 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -3,15 +3,37 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
+	"net/url"
+	"os"
 )
 
 func main() {
-	fmt.Print("hi")
-	connStr := "user=pqgotest dbname=pqgotest sslmode=verify-full"
-	_, err := sql.Open("postgres", connStr)
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error loading .env file")
 	}
+
+	db_user := os.Getenv("DB_USER")
+	db_password := url.QueryEscape(os.Getenv("DB_PASSWORD"))
+	db_host := os.Getenv("DB_HOST")
+	db_name := os.Getenv("DB_NAME")
+	db_SSLMode := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		db_user, db_password, db_host, db_name, db_SSLMode)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error connecting to bible_app: %v", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Error pinging bible_app: %v", err)
+	}
+
+	fmt.Println("Successfully connected to bible_app!")
 }
